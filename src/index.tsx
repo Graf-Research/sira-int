7abb9805de09965f7a610f4001cd28fa5bff558b
Rio on Mac <riochr17@gmail.com>
2022-06-03 11:10:32 +0700

    checkpoint

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const project_pages: string[] = [
   "page Tambah Provinsi\n  [data]\n    row provinsi\n\n  [view]\n    form Formulir Tambah Provinsi\n      - Nama: provinsi.nama text\n\n    button Simpan {\n      confirm Simpan?\n      query 'tambah provinsi' ($1 = provinsi.nama)\n      alert Berhasil menambah provinsi\n      goto Semua Provinsi\n    }\n",
 ];
 
-export const WebDesignerInterpreter = (props: WebDesignerInterpreterProps) => <WebDesignerInterpreter {...props} />;
+export const WebDesignerInterpreter = (props: WebDesignerInterpreterProps) => <_WebDesignerInterpreter {...props} />;
 export class StatementExec extends _StatementExec {};
 
 // Delete me
